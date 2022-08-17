dbfe253f49589dbe9374ff03d40b19845060384d
xzy <xzy@qq.com>
2022-08-17 21:39:09 +0800

    push commit

--- src/main/java/com/xzy/git/GitTest.java
+++ src/main/java/com/xzy/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git3!");
         System.out.println("master git3!");
         System.out.println("hot-fix git3!");
+        System.out.println("push git!");
     }
 }
